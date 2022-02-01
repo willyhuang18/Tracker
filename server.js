fdc1a8162a36b118ba490f73f817161435452bfb
wiily <willyhuang18@gmail.com>
2022-02-01 14:11:55 -0800

    connect to the database

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const mysql = require('mysql2')
 // import inquirer 
 const inquirer = require('inquirer'); 
 // import console.table
-const table = require('console.table'); 
+const table = require('console.table');
+
+//connect to the database
+const db = mysql.createConnection({
+    host:'localhost',
+    user: 'root', 
+    password: 'password',
+    database: ''
+});
