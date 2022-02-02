511a86ebf8222322f7cfd23e61fa246b8dd26b45
wiily <willyhuang18@gmail.com>
2022-02-01 17:12:59 -0800

    create table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
