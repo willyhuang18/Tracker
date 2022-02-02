96f86f5c44aed9ee0a123171d707afa25462894f
wiily <willyhuang18@gmail.com>
2022-02-02 00:25:40 -0800

    adding display content

--- server.js
+++ server.js
@@ -67,5 +67,17 @@ const employeePrompt = () =>{
 //View All Employee function
 viewEmployee = () => {
     console.log('All Employee below:');
-    const sql = ``;
+    const sql = `SELECT employee.id, 
+        employee.first_name, 
+        employee.last_name, 
+        role.title, 
+        department.name AS department,
+        role.salary, 
+        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+        FROM employee
+        LEFT JOIN role ON employee.role_id = role.id
+        LEFT JOIN department ON role.department_id = department.id
+        LEFT JOIN employee manager 
+        ON employee.manager_id = manager.id
+        `;
 }
