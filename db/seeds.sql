94271e086cb8d54df45e9c671dc5d14d8aae4344
wiily <willyhuang18@gmail.com>
2022-02-01 20:46:55 -0800

    insert employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES  ("IT"),
 
 INSERT INTO role (title, salary, department_id)
 VALUES  ("Software Engineer",120000 , 1),
-        ("Human Resources Manager", , 3),
+        ("Human Resources Manager",10000 , 3),
         ("Accountant",100000 ,4 ),
         ("Data Analyst",90000 , 1),
         ("Finanical Analyst",130000 , 4),
@@ -16,3 +16,15 @@ VALUES  ("Software Engineer",120000 , 1),
         ("Product manager", 10000, 2),
         ("Human resource personnel",50000 , 3),
         ("Customer service representative", 50000,2 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Jackson', 'Wang', 1, 1),
+        ('Draven', 'Miller', 9, null),
+        ('Graves', 'Ashe', 4, null),
+        ('Omen', 'Devin', 3, 3),
+        ('Lux', 'LeBlanc', 6, null),
+        ('Akali', 'Akshan', 5, 5),
+        ('Bard', 'Sol', 7, null),
+        ('Cho', 'Gath', 8, 6),
+        ('Mundo', 'Lewis', 2, 8),
+        ('Ezreal', 'Fizz', 10, 7);
