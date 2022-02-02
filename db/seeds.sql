e5395032464557dc9f663b59b8f76e8ac23542d2
wiily <willyhuang18@gmail.com>
2022-02-01 20:36:41 -0800

    insert data for role

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,18 @@
 INSERT INTO department (name)
-VALUES ("IT"),
+VALUES  ("IT"),
         ("Marketing"),
         ("Human Resource"),
         ("Finance"),
         ("Operation Management");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Software Engineer",120000 , 1),
+        ("Human Resources Manager", , 3),
+        ("Accountant",100000 ,4 ),
+        ("Data Analyst",90000 , 1),
+        ("Finanical Analyst",130000 , 4),
+        ("Operating Officer",70000 ,5 ),
+        ("Marketing Manager",70000 , 2),
+        ("Product manager", 10000, 2),
+        ("Human resource personnel",50000 , 3),
+        ("Customer service representative", 50000,2 );
